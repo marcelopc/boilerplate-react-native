0f5b13fa678161b67bf0fb7aac225d542f13b2ca
marcelo <marcelopc04@gmail.com>
2025-07-02 17:20:51 -0300

    react-navigator

---
+++ src/navigation/index.tsx
@@ -0,0 +1,25 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+export const Home = () => {
+  return (
+    <View>
+      <Text>Eu sou a homescreen!</Text>
+    </View>
+  );
+};
+
+export default Home;
