bb581e26f149dfa5de945bff58779cf8fc54dea6
marcelo <marcelopc04@gmail.com>
2025-07-02 15:36:41 -0300

    configuração eslint

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'commonjs',
+      globals: {
+        process: 'readonly',
+        require: 'readonly',
+      },
+    },
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      react: require('eslint-plugin-react-native'),
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // Habilita suporte a JSX
+        },
+        sourceType: 'module', // Corrige para module no parser, já que App.tsx usa ES Modules
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'no-console': 'warn',
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
